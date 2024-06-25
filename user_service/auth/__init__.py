d7c0513c321c9df07aed5cf65c874142b0986e28
g1Den1s-it <g1den1splay@gmail.com>
2024-06-26 00:45:02 +0300

    added config to auth service

--- user_service/auth/__init__.py
+++ user_service/auth/__init__.py
@@ -1,19 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_jwt_extended import JWTManager
+from auth.config import Config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+jwt = JWTManager()
 
 
 def create_app():
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = 'qwdnrtfsgrqdgwerhew12!#@wefqwefwe'
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://anihori:qwerty@db:5432/anihori_db"
+    app.config.from_prefixed_env()
+    app.config.from_object(Config)
 
     db.init_app(app)
     login_manager.init_app(app)
+    jwt.init_app(app)
 
     # service auth
     from auth.routers import auth
---
+++ user_service/auth/config.py
@@ -0,0 +1,9 @@
+import os
+from datetime import timedelta
+
+
+
+class Config:
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(hours=8)
+    JWT_SECRET_KEY = os.getenv("FLASK_JWT_SECRET_KEY")
