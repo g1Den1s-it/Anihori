caebee81b46f5b0c9c2fc3f0372bf1d2dfa7cf58
g1Den1s-it <g1den1splay@gmail.com>
2024-06-28 13:41:26 +0300

    updated

--- user_service/config.py
+++ user_service/config.py
@@ -7,8 +7,14 @@ class Config:
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(hours=8)
     JWT_SECRET_KEY = os.getenv("FLASK_JWT_SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.getenv("FLASK_SQLALCHEMY_DATABASE_URI")
+    DEBUG = os.getenv("FLASK_DEBUG")
+    SECRET_KEY = os.getenv("FLASK_SECRET_KEY")
 
 
 class TestConfig:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
     TESTING = True
+    DEBUG = os.getenv("FLASK_DEBUG")
+    SECRET_KEY = os.getenv("FLASK_SECRET_KEY")
+    JWT_SECRET_KEY = os.getenv("FLASK_JWT_SECRET_KEY")
--- user_service/run.py
+++ user_service/run.py
@@ -2,6 +2,7 @@ from auth import create_app
 from config import Config
 
 if "__main__" == __name__:
-    create_app(Config).run(host='0.0.0.0', port=5000, debug=True)
+    app = create_app(Config)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 
