d0117c98b63a90eefa76c8e9aa1185c9c6bd36c4
g1Den1s-it <g1den1splay@gmail.com>
2024-07-08 19:28:09 +0300

    added models for service

---
+++ video_service/anime/models.py
@@ -0,0 +1,49 @@
+from anime import db
+from sqlalchemy.orm import relationship
+
+
+anime_authors = db.Table(
+    'anime_authors',
+    db.Column('anime_id', db.Integer, db.ForeignKey('anime.id'), primary_key=True),
+    db.Column('author_id', db.Integer, db.ForeignKey('authors.id'), primary_key=True)
+)
+
+anime_genres = db.Table(
+    'anime_genres',
+    db.Column('anime_id', db.Integer, db.ForeignKey('anime.id'), primary_key=True),
+    db.Column('genre_id', db.Integer, db.ForeignKey('genres.id'), primary_key=True)
+)
+
+
+class Genre(db.Model):
+    __tablename__ = 'genres'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
+    name = db.Column(db.String(28), nullable=False)
+
+
+class Author(db.Model):
+    __tablename__ = 'authors'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
+    name = db.Column(db.String(28), nullable=False)
+
+
+class Series(db.Model):
+    __tablename__ = 'series'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
+    name = db.Column(db.String(44), nullable=False)
+    video = db.Column(db.String(164), nullable=False)
+    anime = db.Column(db.Integer, db.ForeignKey('anime.id'), nullable=False)
+
+
+class Anime(db.Model):
+    __tablename__ = 'anime'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
+    title = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    author = relationship('Author', secondary=anime_authors)
+    create_at = db.Column(db.Date)
+    genres = relationship('Genre', secondary=anime_genres)
